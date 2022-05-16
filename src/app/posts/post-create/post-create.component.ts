88ce6facd2544b84893c5f69dfe36dcc9bcd42b8
Abdur-Razzak-Rouzex <abdurrazzak1663@gmail.com>
2022-05-17 00:34:28 +0600

    form is added with validation

--- src/app/posts/post-create/post-create.component.ts
+++ src/app/posts/post-create/post-create.component.ts
@@ -1,5 +1,6 @@
 import {Component, EventEmitter, Output} from '@angular/core';
 import {PostModel} from '../post.model';
+import {NgForm} from '@angular/forms';
 
 
 @Component({
@@ -9,14 +10,15 @@ import {PostModel} from '../post.model';
 })
 
 export class PostCreateComponent {
-  enteredTitle = '';
-  enteredContent = '';
   @Output() postCreated = new EventEmitter<PostModel>();
 
-  onAddPost() {
+  onAddPost(form: NgForm) {
+    if (form.invalid) {
+      return;
+    }
     const post: PostModel = {
-      title: this.enteredTitle,
-      content: this.enteredContent,
+      title: form.value.title,
+      content: form.value.content,
     };
 
     this.postCreated.emit(post);
